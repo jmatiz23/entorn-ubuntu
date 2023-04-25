1933eeb1b470db48c1fc8996de009a84a5baabfa
Oriol Boix Anfosso <orboan@pluralcamp.com>
2023-04-25 07:00:21 +0200

    updated

--- README.md
+++ README.md
@@ -7,14 +7,11 @@
 guiApplications=false
 ```
 
+- Make your user as [passwordless sudoer](https://timonweb.com/devops/how-to-enable-passwordless-sudo-for-a-specific-user-in-linux/)
 - Run install-1.sh
 - Log out and log in again
 - Run install-2.sh
 - Run install-kernels.sh
-
-## Adding desktop
-
-- Run ./installers/install-gui.sh
 
 ## GUI clients for WSL
 
--- installers/install-chrome.sh
+++ installers/install-chrome.sh
@@ -4,8 +4,8 @@ DEBIAN_FRONTEND=noninteractive
 #wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 #sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/sudo apt/sources.list.d/google-chrome.list'
 sudo apt-get -y update
-sudo apt install -y libvulkan1 libu2f-udev 
-sudo apt --fix-broken install
+sudo apt install -y libvulkan1 libu2f-udev libgbm1 
+sudo apt --fix-broken install --yes
 #sudo apt-get install -y --noinstall-recommends google-chrome-stable
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
